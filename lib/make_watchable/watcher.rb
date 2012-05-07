08d1b14c85303633b46bea08ccc4c05c1281609a
Sickboy <sb@dev-heaven.net>
2012-05-07 09:44:26 +0200

    Fixed: Method should be in Watchable not Watcher

--- lib/make_watchable/watcher.rb
+++ lib/make_watchable/watcher.rb
@@ -10,19 +10,6 @@ module MakeWatchable
       def watcher?
         true
       end
-
-      # Method to find the ActiveRecord base class in case of STI
-      # Required due to issue where Polymorphic models list their base class in the association
-      # Thus we should also search for the baseclass when querying
-      def sti_base_class
-        return @sti_base_class unless @sti_base_class.nil?
-        klass = self.class
-        self.class.ancestors.each do |k|
-          break if k == ActiveRecord::Base # we reached the bottom of this barrel
-          klass = k if k.is_a? Class
-        end
-        @sti_base_class = klass
-      end
     end
 
     # Watch a +watchable+.
