17c41abad20130679978e9dc70c192187e47ac00
Sickboy <sb@dev-heaven.net>
2012-05-07 09:39:38 +0200

    Fixed STI and polymorphic uses STI base class in the assocation _type column.

--- lib/make_watchable/watcher.rb
+++ lib/make_watchable/watcher.rb
@@ -10,6 +10,19 @@ module MakeWatchable
       def watcher?
         true
       end
+
+      # Method to find the ActiveRecord base class in case of STI
+      # Required due to issue where Polymorphic models list their base class in the association
+      # Thus we should also search for the baseclass when querying
+      def sti_base_class
+        return @sti_base_class unless @sti_base_class.nil?
+        klass = self.class
+        self.class.ancestors.each do |k|
+          break if k == ActiveRecord::Base # we reached the bottom of this barrel
+          klass = k if k.is_a? Class
+        end
+        @sti_base_class = klass
+      end
     end
 
     # Watch a +watchable+.
@@ -79,7 +92,7 @@ module MakeWatchable
 
     def fetch_watching(watchable)
       watchings.where({
-        :watchable_type => watchable.class.to_s,
+        :watchable_type => watchable.class.sti_base_class.to_s,
         :watchable_id => watchable.id
       }).try(:first)
     end
